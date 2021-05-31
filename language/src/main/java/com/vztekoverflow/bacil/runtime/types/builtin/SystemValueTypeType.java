799b776c46115b6fcc98ac0cbff51fe1e37f0cff
jagotu <jagotu@gmail.com>
2021-05-31 14:50:15 +0200

    Fix warnings
    * use UnsupportedOperationException instead of (sun internal) NotImplementedException
    * remove unused sun.misc.Unsafe wrappers
    * update junit dependency (Dependabot security report)

--- language/src/main/java/com/vztekoverflow/bacil/runtime/types/builtin/SystemValueTypeType.java
+++ language/src/main/java/com/vztekoverflow/bacil/runtime/types/builtin/SystemValueTypeType.java
@@ -4,7 +4,6 @@ import com.vztekoverflow.bacil.parser.cli.CLIComponent;
 import com.vztekoverflow.bacil.parser.cli.tables.generated.CLITypeDefTableRow;
 import com.vztekoverflow.bacil.runtime.types.NamedType;
 import com.vztekoverflow.bacil.runtime.types.locations.LocationsHolder;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class SystemValueTypeType extends NamedType {
     public SystemValueTypeType(CLITypeDefTableRow type, CLIComponent component) {
@@ -18,36 +17,36 @@ public class SystemValueTypeType extends NamedType {
 
     @Override
     public Object initialValue() {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 
     @Override
     public void stackToLocation(LocationsHolder holder, int holderOffset, Object ref, long primitive) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 
     @Override
     public void locationToStack(LocationsHolder holder, int holderOffset, Object[] refs, long[] primitives, int slot) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 
     @Override
     public Object stackToObject(Object ref, long primitive) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 
     @Override
     public void objectToStack(Object[] refs, long[] primitives, int slot, Object value) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 
     @Override
     public Object locationToObject(LocationsHolder holder, int holderOffset) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 
     @Override
     public void objectToLocation(LocationsHolder holder, int holderOffset, Object obj) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 }
