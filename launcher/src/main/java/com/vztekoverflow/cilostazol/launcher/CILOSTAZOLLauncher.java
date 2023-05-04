69a77c32fc6846869767c4db3d823e067f17a3ea
leskovde <leskovar.denis@seznam.cz>
2023-05-04 16:56:25 +0200

    Add invalid DLL handling.

--- launcher/src/main/java/com/vztekoverflow/cilostazol/launcher/CILOSTAZOLLauncher.java
+++ launcher/src/main/java/com/vztekoverflow/cilostazol/launcher/CILOSTAZOLLauncher.java
@@ -3,6 +3,7 @@ package com.vztekoverflow.cilostazol.launcher;
 import org.graalvm.launcher.AbstractLanguageLauncher;
 import org.graalvm.options.OptionCategory;
 import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.PolyglotException;
 import org.graalvm.polyglot.Source;
 
 import java.io.File;
@@ -62,7 +63,10 @@ public class CILOSTAZOLLauncher extends AbstractLanguageLauncher {
             final long done = System.currentTimeMillis();
             System.err.println(LauncherBundle.message("launcher.output.return.time", done));
         } catch (IOException e) {
-            printFileNotFound(inputFile);
+            printFileNotFound();
+        }
+        catch (PolyglotException e) {
+            handlePolyglotException(e);
         }
     }
 
@@ -75,15 +79,48 @@ public class CILOSTAZOLLauncher extends AbstractLanguageLauncher {
     protected void printHelp(OptionCategory maxCategory) {
     }
 
+    private void handlePolyglotException(PolyglotException e) {
+        if (e.isIncompleteSource()) {
+            printIncompleteSource();
+            return;
+        }
+
+        if (e.isInternalError() && e.getMessage().contains("entry point")) {
+            printEntryPointNotFound();
+            return;
+        }
+
+        printPolyglotException(e);
+    }
+
+    private void printPolyglotException(PolyglotException e) {
+        System.err.println("Exception: " + e.getMessage());
+        System.err.println("Stack trace:");
+        e.printStackTrace();
+    }
+
     private void printUsage() {
         System.out.println();
         System.out.println("Usage: cilostazol [options] [path-to-application]");
         printDefaultHelp(OptionCategory.USER);
     }
 
-    private void printFileNotFound(String filePath) {
+    private void printFileNotFound() {
         System.out.println("Could not execute because the specified file was not found.");
         System.out.println("Possible reasons for this include:");
-        System.out.println("\t* You intended to execute a .NET program, but dotnet-" + filePath + " does not exist.");
+        System.out.println("\t* You intended to execute a .NET program, but dotnet-" + inputFile + " does not exist.");
+    }
+
+    private void printEntryPointNotFound() {
+        System.out.println("Could not execute because the specified file does not contain a valid entry point.");
+        System.out.println("Possible reasons for this include:");
+        System.out.println("\t* You intended to execute a .NET program, but " + inputFile + " is not a valid .NET assembly.");
+        System.out.println("\t* You intended to execute a .NET program, but " + inputFile + " is a class library.");
+    }
+
+    private void printIncompleteSource() {
+        System.out.println("Could not execute because the specified file contains an incomplete source.");
+        System.out.println("Possible reasons for this include:");
+        System.out.println("\t* You intended to execute a .NET program, but " + inputFile + " is not a valid .NET assembly.");
     }
 }
