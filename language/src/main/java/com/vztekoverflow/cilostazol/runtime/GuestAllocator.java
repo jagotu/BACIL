2f5aa5170903c55eea554a55fdca4b9da4e22d1e
leskovde <leskovar.denis@seznam.cz>
2023-06-05 19:29:41 +0200

    #8: Separate new allocation and object allocation, if ever needed; track allocation

--- language/src/main/java/com/vztekoverflow/cilostazol/runtime/GuestAllocator.java
+++ language/src/main/java/com/vztekoverflow/cilostazol/runtime/GuestAllocator.java
@@ -1,13 +1,20 @@
 package com.vztekoverflow.cilostazol.runtime;
 
 import com.oracle.truffle.api.CompilerAsserts;
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.instrumentation.AllocationReporter;
+import com.oracle.truffle.api.nodes.ExplodeLoop;
 import com.vztekoverflow.cilostazol.CILOSTAZOLLanguage;
+import com.vztekoverflow.cilostazol.context.ContextAccessImpl;
 import com.vztekoverflow.cilostazol.context.LanguageAccess;
-import com.vztekoverflow.cilostazol.objectmodel.ArrayKlass;
-import com.vztekoverflow.cilostazol.objectmodel.Klass;
-import com.vztekoverflow.cilostazol.objectmodel.ObjectKlass;
+import com.vztekoverflow.cilostazol.exceptions.InstantiationError;
+import com.vztekoverflow.cilostazol.exceptions.InstantiationException;
+import com.vztekoverflow.cilostazol.exceptions.NegativeArraySizeException;
+import com.vztekoverflow.cilostazol.meta.SystemTypes;
 import com.vztekoverflow.cilostazol.objectmodel.StaticObject;
+import com.vztekoverflow.cilostazol.runtime.typesystem.field.IField;
+import com.vztekoverflow.cilostazol.runtime.typesystem.type.CLIType;
+import com.vztekoverflow.cilostazol.runtime.typesystem.type.TypeBase;
 
 public final class GuestAllocator implements LanguageAccess {
     private final CILOSTAZOLLanguage language;
@@ -34,25 +41,171 @@ public final class GuestAllocator implements LanguageAccess {
         return language;
     }
 
-    public StaticObject createClass(Klass klass) {
-        assert klass != null;
-        CompilerAsserts.neverPartOfCompilation();
-        ObjectKlass guestClass = klass.getMeta().system_Type;
-        StaticObject newObj = guestClass.getLinkedKlass().getShape(false).getFactory().create(guestClass);
-        initInstanceFields(newObj, guestClass);
+    /**
+     * Allocates a new instance of the given class; does not call any constructor. Initializes the
+     * class.
+     *
+     * @param type The type of the reference to allocate. If it is PE-constant, the field
+     *             initialization loop can be exploded. This is expected to be the case when
+     *             executing the {@code NEW} bytecode, but may not be the case always.
+     */
+    public StaticObject createNew(TypeBase<?> type) {
+        assert AllocationChecks.canAllocateNewReference(type);
+        type.safelyInitialize();
+        StaticObject newObj = type.getShape(false).getFactory().create(type);
+        initInstanceFields(newObj, type);
+        return trackAllocation(type, newObj);
+    }
 
-        klass.getMeta().java_lang_Class_classLoader.setObject(newObj, klass.getDefiningClassLoader());
-        if (klass.getContext().getJavaVersion().modulesEnabled()) {
-            setModule(newObj, klass);
+    // TODO: This might take constructors into consideration
+    public StaticObject createClass(TypeBase<?> type) {
+        return createNew(type);
+    }
+
+    private static void initInstanceFields(StaticObject obj, TypeBase<?> thisKlass) {
+        if (CompilerDirectives.isPartialEvaluationConstant(thisKlass)) {
+            initLoop(obj, thisKlass);
+        } else {
+            initLoopNoExplode(obj, thisKlass);
         }
-        // The Class.componentType field is only available on 9+.
-        if (klass.isArray() && klass.getMeta().java_lang_Class_componentType != null) {
-            klass.getMeta().java_lang_Class_componentType.setObject(newObj, ((ArrayKlass) klass).getComponentType().initializeEspressoClass());
+    }
+
+    @ExplodeLoop
+    private static void initLoop(StaticObject obj, TypeBase<?> type) {
+        for (IField f : type.getFields()) {
+            assert !f.isStatic();
+            if (f.getKind() == SystemTypes.Object) {
+                f.setObject(obj, StaticObject.NULL);
+            }
         }
-        // Will be overriden if necessary, but should be initialized to non-host null.
-        klass.getMeta().HIDDEN_PROTECTION_DOMAIN.setHiddenObject(newObj, StaticObject.NULL);
-        // Final hidden field assignment
-        klass.getMeta().HIDDEN_MIRROR_KLASS.setHiddenObject(newObj, klass);
-        return trackAllocation(klass, newObj);
+    }
+
+    private static void initLoopNoExplode(StaticObject obj, TypeBase<?> type) {
+        for (IField f : type.getFields()) {
+            assert !f.isStatic();
+            if (f.getKind() == SystemTypes.Object) {
+                f.setObject(obj, StaticObject.NULL);
+            }
+        }
+    }
+
+    private StaticObject trackAllocation(TypeBase<?> type, StaticObject obj) {
+        return trackAllocation(type, obj, getLanguage(), type);
+    }
+
+    private static StaticObject trackAllocation(TypeBase<?> type, StaticObject obj, CILOSTAZOLLanguage lang, ContextAccessImpl contextAccess) {
+        if (type == null || lang.isAllocationTrackingDisabled()) {
+            return obj;
+        }
+        if (!CompilerDirectives.isPartialEvaluationConstant(contextAccess)) {
+            return trackAllocationBoundary(contextAccess, obj);
+        }
+        return contextAccess.getAllocator().trackAllocation(obj);
+    }
+
+    @CompilerDirectives.TruffleBoundary
+    private static StaticObject trackAllocationBoundary(ContextAccessImpl context, StaticObject obj) {
+        return context.getAllocator().trackAllocation(obj);
+    }
+
+    public <T> T trackAllocation(T object) {
+        if (allocationReporter != null) {
+            CompilerAsserts.partialEvaluationConstant(allocationReporter);
+            allocationReporter.onEnter(null, 0, AllocationReporter.SIZE_UNKNOWN);
+            allocationReporter.onReturnValue(object, 0, AllocationReporter.SIZE_UNKNOWN);
+        }
+        return object;
+    }
+
+    public interface AllocationProfiler {
+        AllocationProfiler NO_PROFILE = new AllocationProfiler() {
+            @Override
+            public void enterNewReference() {
+
+            }
+
+            @Override
+            public void enterNewArray() {
+
+            }
+
+            @Override
+            public void enterNewMultiArray() {
+
+            }
+        };
+
+        void enterNewReference();
+
+        void enterNewArray();
+
+        void enterNewMultiArray();
+    }
+
+    public static final class AllocationChecks {
+        private AllocationChecks() {
+        }
+
+        public static void checkCanAllocateNewReference(CLIType type, boolean error) {
+            checkCanAllocateNewReference(type, error, AllocationProfiler.NO_PROFILE);
+        }
+
+        public static void checkCanAllocateArray(int size) {
+            checkCanAllocateArray(size, AllocationProfiler.NO_PROFILE);
+        }
+
+        public static void checkCanAllocateNewReference(CLIType type, boolean error, AllocationProfiler profile) {
+            if (!canAllocateNewReference(type)) {
+                profile.enterNewReference();
+                throw error ? new InstantiationError() : new InstantiationException();
+            }
+        }
+
+        public static void checkCanAllocateArray(int size, AllocationProfiler profile) {
+            if (!canAllocateNewArray(size)) {
+                profile.enterNewArray();
+                throw new NegativeArraySizeException();
+            }
+        }
+
+        private static boolean canAllocateNewReference(CLIType type) {
+            return (type instanceof TypeBase<?>) && !type.isAbstract() && !type.isInterface();
+        }
+
+        private static boolean canAllocateNewArray(int size) {
+            return size >= 0;
+        }
+
+        private static boolean invalidDimensionsArray(int[] dimensions) {
+            return dimensions.length == 0 || dimensions.length > 255;
+        }
+
+        @ExplodeLoop
+        private static boolean invalidDimensions(int[] dimensions) {
+            if (CompilerDirectives.isPartialEvaluationConstant(dimensions)) {
+                return invalidDimensionsExplode(dimensions);
+            } else {
+                return invalidDimensionsNoExplode(dimensions);
+            }
+        }
+
+        private static boolean invalidDimensionsNoExplode(int[] dimensions) {
+            for (int dim : dimensions) {
+                if (!canAllocateNewArray(dim)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @ExplodeLoop
+        private static boolean invalidDimensionsExplode(int[] dimensions) {
+            for (int dim : dimensions) {
+                if (!canAllocateNewArray(dim)) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
