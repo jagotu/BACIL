d2f51ccfd1e3de083112c65c2c44770be20f8796
leskovde <leskovar.denis@seznam.cz>
2023-06-05 21:54:57 +0200

    #8: Disable Meta

--- language/src/main/java/com/vztekoverflow/cilostazol/meta/Meta.java
+++ language/src/main/java/com/vztekoverflow/cilostazol/meta/Meta.java
@@ -2,25 +2,29 @@ package com.vztekoverflow.cilostazol.meta;
 
 import com.oracle.truffle.api.CompilerAsserts;
 import com.vztekoverflow.cilostazol.context.ContextAccessImpl;
-import com.vztekoverflow.cilostazol.objectmodel.ArrayKlass;
-import com.vztekoverflow.cilostazol.objectmodel.ObjectKlass;
-import com.vztekoverflow.cilostazol.objectmodel.ValueKlass;
 import com.vztekoverflow.cilostazol.runtime.CILOSTAZOLContext;
+import com.vztekoverflow.cilostazol.runtime.typesystem.type.CLIType;
+import com.vztekoverflow.cilostazol.runtime.typesystem.type.TypeBase;
 
 public final class Meta extends ContextAccessImpl {
-    public final ObjectKlass system_Object;
-    public final ArrayKlass system_Object_array;
+    public Meta(CILOSTAZOLContext context) {
+        super(context);
+    }
 
-    public final ObjectKlass system_String;
-    public final ObjectKlass system_Type;
+    /*
+    public final TypeBase<?> system_Object;
+    public final TypeBase<?> system_Object_array;
 
-    public final ValueKlass _boolean;
-    public final ValueKlass _char;
-    public final ValueKlass _float;
-    public final ValueKlass _int;
-    public final ValueKlass _double;
-    public final ValueKlass _long;
-    public final ValueKlass _void;
+    public final TypeBase<?> system_String;
+    public final TypeBase<?> system_Type;
+
+    public final CLIType _boolean;
+    public final CLIType _char;
+    public final CLIType _float;
+    public final CLIType _int;
+    public final CLIType _double;
+    public final CLIType _long;
+    public final CLIType _void;
 
     public Meta(CILOSTAZOLContext context) {
         super(context);
@@ -32,23 +36,6 @@ public final class Meta extends ContextAccessImpl {
         // Core types.
         // Object and Class (+ Class fields) must be initialized before all other classes in order
         // to eagerly create the guest Class instances.
-
-        // TODO:
-        system_Object = new ObjectKlass();
-
-        system_Object_array = system_Object.array();
-
-        // TODO:
-        system_String = new ObjectKlass();
-        // TODO:
-        system_Type = new ObjectKlass();
-
-        _boolean = new ValueKlass(context, SystemTypes.Boolean);
-        _char = new ValueKlass(context, SystemTypes.Char);
-        _float = new ValueKlass(context, SystemTypes.Float);
-        _int = new ValueKlass(context, SystemTypes.Int);
-        _double = new ValueKlass(context, SystemTypes.Double);
-        _long = new ValueKlass(context, SystemTypes.Long);
-        _void = new ValueKlass(context, SystemTypes.Void);
     }
+    */
 }
