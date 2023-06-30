353880afbfccebca7d4dd679d14142a65fd85380
Tomas Husak <husaktomas98@gmail.com>
2023-06-30 16:53:34 +0200

    create tests for interpreter

--- tests/src/test/java/com/vztekoverflow/cilostazol/tests/BasicTests.java
+++ tests/src/test/java/com/vztekoverflow/cilostazol/tests/BasicTests.java
@@ -5,8 +5,12 @@ import com.vztekoverflow.cilostazol.launcher.CILOSTAZOLLauncher;
 public class BasicTests extends TestBase {
   public void test() {}
 
-  public void _testInitial() {
+  public void testInitial() {
     CILOSTAZOLLauncher launcher = new CILOSTAZOLLauncher();
+
     launcher.test(new String[] {getDllPath("Initial").toString()});
+    int returnValue = launcher.getReturnValue();
+
+    assertEquals(1, returnValue);
   }
 }
