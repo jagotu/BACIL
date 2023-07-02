1f7a262b1788bc0897e5ca183cd08836ef135517
Tomas Husak <husaktomas98@gmail.com>
2023-07-02 15:09:55 +0200

    fix after merge

--- language/src/test/java/com/vztekoverflow/cilostazol/runtime/typesystem/PrimitiveTypesParsingTests.java
+++ language/src/test/java/com/vztekoverflow/cilostazol/runtime/typesystem/PrimitiveTypesParsingTests.java
@@ -1,7 +1,7 @@
 package com.vztekoverflow.cilostazol.runtime.typesystem;
 
 import com.vztekoverflow.cil.parser.cli.AssemblyIdentity;
-import com.vztekoverflow.cilostazol.runtime.CILOSTAZOLContext;
+import com.vztekoverflow.cilostazol.runtime.context.CILOSTAZOLContext;
 import com.vztekoverflow.cilostazol.runtime.symbols.ArrayTypeSymbol;
 import com.vztekoverflow.cilostazol.runtime.context.CILOSTAZOLContext;
 import com.vztekoverflow.cilostazol.runtime.symbols.NamedTypeSymbol;
