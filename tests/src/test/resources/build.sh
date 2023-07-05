5b71c02c782434ecf1df770eb1d90dac51aaa915
Tomas Husak <husaktomas98@gmail.com>
2023-07-05 11:43:40 +0200

    fix linux paths

--- tests/src/test/resources/build.sh
+++ tests/src/test/resources/build.sh
@@ -7,5 +7,5 @@ projects=$(find $SCRIPT_DIR -name '*.csproj')
 for project in $projects; do
 	echo $project
 	projectdir="$(dirname "$project")"
-	dotnet build -c=Release "${projectdir}\bin"
+	dotnet build -c=Release --output "${projectdir}\bin"
 done
