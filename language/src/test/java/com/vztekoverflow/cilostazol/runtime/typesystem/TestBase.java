2eb0d20ea74588829f254c80bd41b5ccd096e1dc
Tomas Husak <husaktomas98@gmail.com>
2023-07-05 10:19:23 +0200

    fix paths

--- language/src/test/java/com/vztekoverflow/cilostazol/runtime/typesystem/TestBase.java
+++ language/src/test/java/com/vztekoverflow/cilostazol/runtime/typesystem/TestBase.java
@@ -25,11 +25,7 @@ public abstract class TestBase extends TestCase {
   protected CILOSTAZOLLanguage lang;
 
   protected Path getDllPath(String projectName) {
-    return Paths.get(
-        _directory,
-        projectName,
-        String.format("bin/%s/%s", _configuration, _dotnetVersion),
-        projectName + ".dll");
+    return Paths.get(_directory, projectName, "bin", projectName + ".dll");
   }
 
   protected Source getSourceFromProject(String projectName) throws IOException {
--- language/src/test/resources/StaticObjectModelTestTargets/build.sh
+++ language/src/test/resources/StaticObjectModelTestTargets/build.sh
@@ -6,5 +6,5 @@ projects=$(find $SCRIPT_DIR -name '*.csproj')
 
 for project in $projects; do
 	echo $project
-	dotnet build -c=Release $project
+	dotnet build -c=Release "${project}\..\bin"
 done
