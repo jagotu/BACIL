615705fcc1d0734d6572c01aa20a2fafeaae9998
leskovde <leskovar.denis@seznam.cz>
2023-07-20 20:04:30 +0200

    #60: Tests for less-than

--- tests/src/test/java/com/vztekoverflow/cilostazol/tests/StackOperationTests.java
+++ tests/src/test/java/com/vztekoverflow/cilostazol/tests/StackOperationTests.java
@@ -26,4 +26,120 @@ public class StackOperationTests extends TestBase {
 
     assertEquals(1, result.exitCode());
   }
+
+  @Test
+  public void binaryLessThanInt32() {
+    var result =
+        runTestFromCode(
+            """
+              using System;
+              namespace CustomTest
+              {
+                public class Program
+                {
+                    public static int Main()
+                    {
+                        int a = 42;
+                        if (a < 43)
+                        {
+                            return 1;
+                        }
+                        else
+                        {
+                            return 0;
+                        }
+                    }
+                }
+              }
+                """);
+
+    assertEquals(1, result.exitCode());
+  }
+
+  @Test
+  public void binaryLessThanInt64() {
+    var result =
+        runTestFromCode(
+            """
+              using System;
+              namespace CustomTest
+              {
+                public class Program
+                {
+                    public static int Main()
+                    {
+                        long a = 42_000_000_000_000_000L;
+                        if (a < 43_000_000_000_000_000L)
+                        {
+                            return 1;
+                        }
+                        else
+                        {
+                            return 0;
+                        }
+                    }
+                }
+              }
+                """);
+
+    assertEquals(1, result.exitCode());
+  }
+
+  @Test
+  public void binaryLessThanFloat32() {
+    var result =
+        runTestFromCode(
+            """
+                    using System;
+                    namespace CustomTest
+                    {
+                    public class Program
+                    {
+                        public static int Main()
+                        {
+                            float a = 1.0f;
+                            if (a < 43.0f)
+                            {
+                                return 1;
+                            }
+                            else
+                            {
+                                return 0;
+                            }
+                        }
+                    }
+                    }
+                    """);
+
+    assertEquals(1, result.exitCode());
+  }
+
+  @Test
+  public void binaryLessThanFloat64() {
+    var result =
+        runTestFromCode(
+            """
+                    using System;
+                    namespace CustomTest
+                    {
+                    public class Program
+                    {
+                        public static int Main()
+                        {
+                            double a = 1.0;
+                            if (a < 43.0d)
+                            {
+                                return 1;
+                            }
+                            else
+                            {
+                                return 0;
+                            }
+                        }
+                    }
+                    }
+                    """);
+
+    assertEquals(1, result.exitCode());
+  }
 }
